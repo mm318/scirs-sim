3f67519a5d386fbaa9413e6d28d2270ba07b77ec
Miaofei <mfmei318@gmail.com>
2021-01-04 00:57:24 -0800

    add graph traversal for more efficient model evaluation
    Signed-off-by: Miaofei <mfmei318@gmail.com>

--- src/core/src/dag/mod.rs
+++ src/core/src/dag/mod.rs
@@ -1,4 +1,4 @@
-// Taken from https://github.com/bunker-inspector/rs_taskflow/tree/master/src/flow/dag
+// Based on https://github.com/bunker-inspector/rs_taskflow/tree/master/src/flow/dag
 
 pub mod dag;
 pub mod node;
