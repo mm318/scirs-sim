67325f76cf12b00aa6ac226cadb52290ea3b4b8b
Miaofei <mfmei318@gmail.com>
2021-02-06 01:35:27 -0800

    update basic implementation
    Signed-off-by: Miaofei <mfmei318@gmail.com>

--- src/core/src/model.rs
+++ src/core/src/model.rs
@@ -7,57 +7,62 @@ pub trait BlockType {
     type BlockType;
 }
 
-pub struct BlockId<T> {
-    block_idx: usize,
+pub struct BlockHandle<T> {
+    block_id: usize,
     data_type: PhantomData<T>,
 }
 
-impl<T> BlockType for BlockId<T> {
+impl<T> BlockType for BlockHandle<T> {
     type BlockType = T;
 }
 
-impl<T> BlockId<T> {
-    pub fn idx(&self) -> usize {
-        return self.block_idx;
+impl<T> BlockHandle<T> {
+    pub fn id(&self) -> usize {
+        return self.block_id;
     }
 }
 
-// #[derive(Debug)]
 pub struct Model {
     blocks: Vec<Box<dyn Block>>,
 }
+
+// struct ModelTraversal {
+//     dag: Dag<'a, &'a dyn Block>,
+//     ready: VecDeque<&'a Node<'a, &'a dyn Block>>,
+// }
 
 impl Model {
     pub fn new() -> Self {
         return Model { blocks: Vec::new() };
     }
 
-    pub fn add_block<B: Block>(&mut self, new_block: B) -> BlockId<B> {
-        let idx = self.blocks.len();
+    pub fn add_block<B: Block>(&mut self, new_block: B) -> BlockHandle<B> {
+        let id = self.blocks.len();
         self.blocks.push(Box::new(new_block));
-        return BlockId {
-            block_idx: idx,
+        return BlockHandle {
+            block_id: id,
             data_type: PhantomData,
         };
     }
 
-    pub fn get_block<B>(&self, block_id: &BlockId<B>) -> &dyn Block {
-        return self.get_block_by_idx(block_id.idx());
+    pub fn get_block_by_id(&self, block_id: usize) -> &dyn Block {
+        return &*self.blocks[block_id];
     }
 
-    pub fn get_block_by_idx(&self, block_idx: usize) -> &dyn Block {
-        return &*self.blocks[block_idx];
+    pub fn get_block<B>(&self, block_handle: &BlockHandle<B>) -> &dyn Block {
+        return self.get_block_by_id(block_handle.id());
     }
 
-    fn get_concrete_block<B: Any>(&self, block_id: &BlockId<B>) -> &B {
-        return self.blocks[block_id.idx()]
+    fn get_concrete_block<B: Any>(&self, block_handle: &BlockHandle<B>) -> &B {
+        return self
+            .get_block(block_handle)
             .as_any()
             .downcast_ref::<B>()
             .unwrap();
     }
 
-    fn get_mut_concrete_block<B: Any>(&mut self, block_id: &BlockId<B>) -> &mut B {
-        return self.blocks[block_id.idx()]
+    fn get_mut_concrete_block<B: Any>(&mut self, block_handle: &BlockHandle<B>) -> &mut B {
+        return self.blocks[block_handle.id()]
             .as_mut_any()
             .downcast_mut::<B>()
             .unwrap();
@@ -65,40 +70,30 @@ impl Model {
 
     pub fn connect<A: Block, B: Block, T: Default>(
         &mut self,
-        block1_handle: &BlockId<A>,
+        block1_handle: &BlockHandle<A>,
         block1_output: fn(&dyn Block) -> T,
-        block2_handle: &BlockId<B>,
+        block2_handle: &BlockHandle<B>,
         block2_input: fn(&mut B, BlockInput<T>),
     ) {
         (block2_input)(
             self.get_mut_concrete_block(block2_handle),
-            BlockInput::new(block1_handle.idx(), block1_output),
+            BlockInput::new(block1_handle.id(), block1_output),
         );
     }
 
     pub fn exec(&self, steps: usize) {
-        // debug
-        self.debug_state("step 0");
-        println!();
-
         for step in 0..steps {
+            // debug
+            println!("\nstep {}", step + 1);
+
             for block in &self.blocks {
+                println!("  Visiting {:?}", block);
                 block.calc(self);
             }
+
             for block in &self.blocks {
                 block.update();
             }
-
-            // debug
-            self.debug_state(format!("step {}", step + 1).as_str());
-            println!();
-        }
-    }
-
-    fn debug_state(&self, state_id: &str) {
-        println!("{}:", state_id);
-        for (i, block) in self.blocks.iter().enumerate() {
-            println!("  block_id {} = {:?}", i, block);
         }
     }
 }
